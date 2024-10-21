3ebf0616f7e417ada684ab6a80821deecfcefbb6
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2024-10-21 13:03:10 +0530

    added

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input --email "nilam@gmail.com" --username "nilam"
+  python manage.py createsuperuser --no-input --email "nilam@gmail.com" 
 fi
